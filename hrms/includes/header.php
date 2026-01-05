2663c4b89279d75eae864f48458dcf1b2fefca8a
andrewjimenezz <andrewtimothyjimenez2005@gmail.com>
2026-01-05 16:03:36 +0800

    Add files via upload

---
+++ hrms/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- GLOBAL SIDEBAR CSS -->
+    <link rel="stylesheet" href="../public/css/sidebar.css">
